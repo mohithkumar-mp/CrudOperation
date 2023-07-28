3152a47b74531627e669b5b6e93acd50882e4aca
mohithkumar-mp <mohithmp778@gmail.com>
2023-07-29 00:13:56 +0530

    added bootsrtap

---
+++ src/user.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+export default function User() {
+  return (
+    <div>
+        <Button>This is User Button</Button>
+    </div>
+  )
+}
